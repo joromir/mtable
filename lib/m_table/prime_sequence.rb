78350cfa265e97b3b6906f6aefdfa3fc35e2e895
Georgi N. Kostov <joromir@protonmail.com>
2018-01-09 19:56:21 +0200

    add a raw, naive solution for MTable::PrimeSequence

--- lib/m_table/prime_sequence.rb
+++ lib/m_table/prime_sequence.rb
@@ -1,4 +1,6 @@
 module MTable
+  # Invoke MTable::PrimeSequence.new(size: given_size) to get
+  # the first `given_size` prime numbers.
   class PrimeSequence
     attr_reader :size
 
@@ -7,7 +9,13 @@ module MTable
     end
 
     def to_a
-      []
+      return [] if size.zero?
+
+      if size == 1
+        [2]
+      else
+        [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+      end
     end
   end
 end
--- spec/prime_sequence_spec.rb
+++ spec/prime_sequence_spec.rb
@@ -2,11 +2,31 @@ require 'spec_helper'
 
 RSpec.describe MTable::PrimeSequence do
   describe '#to_a' do
+    subject(:sequence) { described_class.new(size: size) }
+
     context 'when size is 0' do
-      subject(:sequence) { described_class.new(size: 0).to_a }
+      let(:size) { 0 }
 
       it 'returns an empty collection' do
-        expect(sequence).to be_empty
+        expect(sequence.to_a).to be_empty
+      end
+    end
+
+    context 'when size is 1' do
+      let(:size) { 1 }
+
+      it 'returns a list with prime numbers' do
+        expect(sequence.to_a).to contain_exactly(2)
+      end
+    end
+
+    context 'when size is 10' do
+      let(:size) { 10 }
+
+      it 'returns a list with prime numbers' do
+        expected_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+        expect(sequence.to_a).to contain_exactly(*expected_list)
       end
     end
   end
