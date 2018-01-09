9462b022a156c0dbd5477e9de97d6987a0033990
Georgi N. Kostov <joromir@protonmail.com>
2018-01-09 23:20:51 +0200

    Refactor MTable::PrimeSequence#to_a

--- lib/m_table/prime_sequence.rb
+++ lib/m_table/prime_sequence.rb
@@ -2,21 +2,22 @@ module MTable
   # Invoke MTable::PrimeSequence.new(size: given_size) to get
   # the first `given_size` prime numbers.
   class PrimeSequence
-    attr_reader :size, :list
+    attr_reader :size
 
     def initialize(size:)
       @size = size
-      @list = []
     end
 
     def to_a
       return [] if size.zero?
 
-      if size == 1
-        [2]
-      else
-        [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+      primes = [2]
+
+      (size - 1).times do
+        primes << NearestPrimeNumber.new(primes.last.succ).to_i
       end
+
+      primes
     end
   end
 end
