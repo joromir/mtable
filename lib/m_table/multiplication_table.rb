4e0a68c0fdc159b88b25b243a530eebb0dd1b5a3
Georgi N. Kostov <joromir@protonmail.com>
2018-01-12 18:55:26 +0200

    add an ugly solution for MTable::MultiplicationTable

--- lib/m_table/multiplication_table.rb
+++ lib/m_table/multiplication_table.rb
@@ -1,26 +1,21 @@
 module MTable
   class MultiplicationTable # :nodoc:
-    attr_reader :integer_list
+    attr_reader :integer_list, :max
 
     def initialize(integer_list)
       @integer_list = integer_list
+      @max          = integer_list.max
     end
 
     def to_s
       return '' if integer_list.empty?
 
-      if integer_list.size == 1
-        "   | 2 \n"\
-        "---+---\n"\
-        " 2 | 4 \n"
-      else
-        "    |  2  3  5  7  11 \n"\
-        "----+----------------\n"\
-        "  2 |  4  6 10 14  22\n"\
-        "  3 |  6  9 15 21  33\n"\
-        "  5 | 10 15 25 35  55\n"\
-        "  7 | 14 21 35 49  77\n"\
-        " 11 | 22 33 55 77 121\n"
+      head = "#{header}\n#{separator}\n"
+
+      matrix.each.with_index.reduce(head) do |acc, (elem, position)|
+        item = beautify(integer_list[position])
+
+        "#{acc}#{item} |#{align_elements(elem)}\n"
       end
     end
 
@@ -32,6 +27,30 @@ module MTable
 
     def matrix_row(elem)
       integer_list.map { |integer| integer * elem }
+    end
+
+    def beautify(number)
+      ' ' * (offset - number.to_s.size) + number.to_s
+    end
+
+    def header
+      ' ' * (offset + 2) + "#{align_elements(integer_list)}"
+    end
+
+    def separator
+      "#{number_space}+#{number_space * integer_list.size}"
+    end
+
+    def number_space
+      '-' * (max.to_s.size + 2)
+    end
+
+    def align_elements(column)
+      column.reduce('') { |acc, elem| "#{acc} #{beautify(elem)}" }
+    end
+
+    def offset
+      @offset ||= (max * max).to_s.size
     end
   end
 end
--- spec/multiplication_table_spec.rb
+++ spec/multiplication_table_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe MTable::MultiplicationTable do
 
     context 'when the input is an array of size one' do
       it 'generates a 1x1 table' do
-        table = "   | 2 \n"\
+        table = "     2\n"\
                 "---+---\n"\
-                " 2 | 4 \n"
+                " 2 | 4\n"
 
         expect(described_class.new([2]).to_s).to eq table
       end
@@ -20,15 +20,34 @@ RSpec.describe MTable::MultiplicationTable do
 
     context 'when the input is an array of size five' do
       it 'generates a 5x5 table' do
-        table = "    |  2  3  5  7  11 \n"\
-                "----+----------------\n"\
-                "  2 |  4  6 10 14  22\n"\
-                "  3 |  6  9 15 21  33\n"\
-                "  5 | 10 15 25 35  55\n"\
-                "  7 | 14 21 35 49  77\n"\
-                " 11 | 22 33 55 77 121\n"
+        table = "        2   3   5   7  11\n"\
+                "----+--------------------\n"\
+                "  2 |   4   6  10  14  22\n"\
+                "  3 |   6   9  15  21  33\n"\
+                "  5 |  10  15  25  35  55\n"\
+                "  7 |  14  21  35  49  77\n"\
+                " 11 |  22  33  55  77 121\n"
 
         expect(described_class.new([2, 3, 5, 7, 11]).to_s).to eq table
+      end
+    end
+
+    context 'when the input is an array of size ten' do
+      it 'generates a 10x10 table' do
+        table = "        2   3   5   7  11  13  17  19  23  29\n"\
+                "----+----------------------------------------\n"\
+                "  2 |   4   6  10  14  22  26  34  38  46  58\n"\
+                "  3 |   6   9  15  21  33  39  51  57  69  87\n"\
+                "  5 |  10  15  25  35  55  65  85  95 115 145\n"\
+                "  7 |  14  21  35  49  77  91 119 133 161 203\n"\
+                " 11 |  22  33  55  77 121 143 187 209 253 319\n"\
+                " 13 |  26  39  65  91 143 169 221 247 299 377\n"\
+                " 17 |  34  51  85 119 187 221 289 323 391 493\n"\
+                " 19 |  38  57  95 133 209 247 323 361 437 551\n"\
+                " 23 |  46  69 115 161 253 299 391 437 529 667\n"\
+                " 29 |  58  87 145 203 319 377 493 551 667 841\n"
+
+        expect(described_class.new([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]).to_s).to eq table
       end
     end
   end
